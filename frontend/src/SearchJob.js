77159ec55d84cd72eae9c0591cf36c2240798ed4
AaraizHassan <113174720+AaraizHassan@users.noreply.github.com>
2023-04-19 00:57:10 +0500

    Add files via upload

---
+++ frontend/src/SearchJob.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import JobSearch from './components/JobSearch';
+
+function App() {
+  return (
+    <div>
+      <JobSearch />
+    </div>
+  );
+}
+
+export default App;
