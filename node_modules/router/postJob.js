e5f6351d12c5db017de0a6076fff81336a55369d
Muhammad Umer Malik <106489578+umer901@users.noreply.github.com>
2023-04-20 06:58:41 +0500

    Add files via upload

---
+++ node_modules/router/postJob.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const Data = require('../model/postJob');
+
+router.post('/submitjob', (req, res) => {
+  const newData = new Data({
+    cnic: req.body.cnic, // changed from req.body.textValue
+    regNumber: req.body.registrationNumber, // changed from req.body.textValue1
+    industry: req.body.industry, // changed from req.body.dropdownValue1
+    location: req.body.location, // changed from req.body.dropdownValue2
+    jobLevel: req.body.jobLevel, // changed from req.body.dropdownValue3
+    salary: req.body.salary, // changed from req.body.dropdownValue4
+  });
+  
+  newData.save().then(() => {
+    res.status(200).send('Data saved successfully');
+  }).catch((err) => {
+    res.status(400).send('Data save error:', err);
+  });
+});
+
+module.exports = router;
